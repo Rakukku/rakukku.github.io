f56ba19a95204dff06b8bbfdf83706133397182b
Mathias Sebastian <mathiassbastian@gmail.com>
2019-11-19 09:42:00 +0700

    SUSAH BANGET

---
+++ precache-manifest.c798852fab7c4df2b0b93da45689e9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca77459b74a109aa7416",
+    "url": "/css/chunk-vendors.ca4425db.css"
+  },
+  {
+    "revision": "4bad6a3163e3446e28d1ef4516070c33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d83f94a37f4fbd0c2bda",
+    "url": "/js/app.02672e77.js"
+  },
+  {
+    "revision": "ca77459b74a109aa7416",
+    "url": "/js/chunk-vendors.8edfba20.js"
+  },
+  {
+    "revision": "a82ade92d01f9636fc9145161f46258e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
